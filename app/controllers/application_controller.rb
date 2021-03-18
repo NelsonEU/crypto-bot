01b7cfdd8666a5839ba5ded50f613110df7e7a74
aetienne <prospectio@Prospectios-MacBook-Pro.local>
2021-03-18 21:33:30 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,36 +1,3 @@
 class ApplicationController < ActionController::API
-  def front
-    session[:plan] = params[:plan] if params[:plan]
 
-    render html: front_html.html_safe
-  end
-
-  private
-
-  def front_html
-    Rails.env.production? ? Front::HTML_CACHE : Front.html
-  end
-
-  def normalized_params
-    DefaultDeserializer.call(params.to_unsafe_hash[:data])
-  end
-
-  def render_errors(errors, status = :unprocessable_entity)
-    render json: Api::Private::ErrorSerializer.new(errors), status: status
-  end
-
-  def render_nothing(status = :ok)
-    render json: { meta: {} }, status: status # see: https://jsonapi.org/format/#crud-deleting-responses-200
-  end
-
-  # Defines a deserializer with the default options
-  class DefaultDeserializer < JSONAPI::Deserializable::Resource
-    key_format { |key| key.downcase.underscore }
-
-    id
-
-    has_one
-    has_many
-    attributes
-  end
 end
